8d50b82d76af8ca524bbef8cb6a748cdc2b4c787
Faezeh-gh <139226352+Faezeh-gh@users.noreply.github.com>
2023-08-31 23:28:04 +0330

    New options added by json.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import datetime as dt
 import requests
 
 
+Base_url = "https://api.openweathermap.org/data/2.5/weather?q="
 API_key = "35566f885b8f003dda9abfbe515e6722"
 
-user_input = input("Enter the city's name: ")
+city = "Tehran"
 
-weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q="
-                            f"{user_input}&units=imperial&APPID={API_key}")
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) / 1.8
+    return celsius
 
-if weather_data.json()['cod'] == '404':
-    print("No City Found")
-else:
-    weather = weather_data.json()['weather'][0]['main']
-    temp = round(weather_data.json()['main']['temp'])
+url = (f"https://api.openweathermap.org/data/2.5/weather?q="
+                            f"{city}&units=imperial&APPID={API_key}")
 
-    print(f"The weather in {user_input} is: {weather}")
-    print(f"The temperature in {user_input} is: {temp}ºF")
+response = requests.get(url).json()
 
+temp_celsius = fahrenheit_to_celsius(response['main']['temp'])
 
+feel_like_celsius = fahrenheit_to_celsius(response['main']['feels_like'])
 
+windspeed = response['wind']['speed']
 
+humidity = response['main']['humidity']
+
+description = response['weather'][0]['description']
+
+sunrise = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+sunset = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+print(f"tempreture in {city}: {temp_celsius:.2f}C")
+print(f"tempreture in {city}: feels like: {feel_like_celsius:.2f}C")
+print(f"tempreture in {city}: {temp_celsius:.2f}C")
+print(f"humidity in {city}: {humidity}%")
+print(f"wind speed in {city}: {windspeed}m/s")
+print(f"general weather in {city}: {description}")
+print(f"sun rises in {city} at {sunrise} local time.")
+print(f"sun sets in {city} at {sunset} local time.")
