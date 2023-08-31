56fb3c950a0e14208954e7e89abd7eec8c39bc38
Faezeh-gh <139226352+Faezeh-gh@users.noreply.github.com>
2023-08-31 22:49:40 +0330

    A simple code to check the weather

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+
+
+API_key = "35566f885b8f003dda9abfbe515e6722"
+
+user_input = input("Enter the city's name: ")
+
+weather_data = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q="
+                            f"{user_input}&units=imperial&APPID={API_key}")
+
+if weather_data.json()['cod'] == '404':
+    print("No City Found")
+else:
+    weather = weather_data.json()['weather'][0]['main']
+    temp = round(weather_data.json()['main']['temp'])
+
+    print(f"The weather in {user_input} is: {weather}")
+    print(f"The temperature in {user_input} is: {temp}ºF")
+
+
+
+
