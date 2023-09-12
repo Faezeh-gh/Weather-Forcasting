97c72be8b431356d8875d45edb304d08c2472f83
Faezeh-gh <139226352+Faezeh-gh@users.noreply.github.com>
2023-09-12 22:01:51 +0330

    fix positions

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ def get_temperature():
     if city:
         api_key = "35566f885b8f003dda9abfbe515e6722"
         url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
-
         try:
             response = requests.get(url)
             weather_data = response.json()
@@ -42,28 +41,31 @@ def get_temperature():
 
 
 window = tk.Tk()
-window.title("Weather Forcasting")
+window.title("Weather Forecasts")
+
+content_frame = tk.Frame(window)
+content_frame.pack(pady=10)
 
 initial_image = Image.open("day.jpg")
 initial_photo = ImageTk.PhotoImage(initial_image)
 
-background_label = tk.Label(window, image=initial_photo)
-background_label.pack()
+background_label = tk.Label(content_frame, image=initial_photo)
+background_label.grid(row=0, column=0, columnspan=2)
 
-change_button = tk.Button(window, text="Change theme", command=change_theme, background="yellow")
-change_button.pack()
+change_button = tk.Button(content_frame, text="Change theme", command=change_theme, background="blue")
+change_button.grid(row=1, column=0, pady=5)
 
-search_frame = tk.Frame(window, bg="white")
-search_frame.pack(fill="x", padx=10, pady=(0, 10))
+search_frame = tk.Frame(content_frame)
+search_frame.grid(row=2, column=0, padx=10)
 
-search_entry = tk.Entry(search_frame)
-search_entry.pack(side="left", expand=True, padx=(0, 5))
+search_entry = tk.Entry(search_frame, font=("Arial", 14))
+search_entry.grid(row=0, column=0, padx=10)
 
 search_btn = tk.Button(search_frame, text="Search", command=get_temperature)
-search_btn.pack(side="right")
+search_btn.grid(row=0, column=1, padx=5)
 
-weather_label = tk.Label(window, text="", font=("Arial", 14))
-weather_label.pack(pady=10)
+weather_label = tk.Label(content_frame, text="", font=("Arial", 16))
+weather_label.grid(row=3, column=0, pady=10)
 
 
 window.mainloop()
